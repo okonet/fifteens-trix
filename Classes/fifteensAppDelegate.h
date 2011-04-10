9da7e7866127d304b775167d3916d7563124a041
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2011-04-10 14:46:11 +0200

    Initial PhoneGap project

---
+++ Classes/fifteensAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  fifteensAppDelegate.h
+//  fifteens
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface fifteensAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
