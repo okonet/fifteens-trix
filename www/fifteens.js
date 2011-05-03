7597b1aad711099542347f43052ab99829fd1d86
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2011-05-04 00:04:44 +0200

    Doctype and events chnaged

--- www/fifteens.js
+++ www/fifteens.js
@@ -64,7 +64,7 @@ $(function(){
     events: {
       'tap' : 'playTile',
       'swipe' : 'playTile',
-      // 'click' : 'playTile',
+      'click' : 'playTile',
     },
     
     initialize: function() {
