b32a861f3c1987047c354224dd95f9afd4bc8aff
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2011-05-23 23:31:09 +0200

    Better folder structure

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
 sass_dir = "app/styleshhets"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "assets/css"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
