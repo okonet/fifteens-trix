6e907098cef39a231b98d7b2e12449186ed763a2
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2012-02-18 15:09:53 +0100

    Updated compass config

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "app/stylesheets"
-css_dir = "public/css"
+css_dir = "/css"
 images_dir = "/img"
-javascripts_dir = "public/js"
+javascripts_dir = "/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
