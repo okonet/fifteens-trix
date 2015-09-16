89fbb0b304f669493f1581fb5fabfafc775b07bb
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-09-16 11:48:11 +0200

    Moved randomizing logic to action

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,22 +1,31 @@
-export const NEW_GAME = 'NEW_GAME';
-export const GAME_OVER = 'GAME_OVER';
-export const INCREMENT_STEPS = 'INCREMENT_STEPS';
+import random from 'random-seed'
+import {range} from 'lodash'
+import { COLS, ROWS, SIZE } from '../constants/Board'
+
+const rand = random.create()
+const startRow = (ROWS - COLS)
+
+export const NEW_GAME = 'NEW_GAME'
+export const GAME_OVER = 'GAME_OVER'
+export const INCREMENT_STEPS = 'INCREMENT_STEPS'
 
 export function incrementSteps() {
     return {
         type: INCREMENT_STEPS
-    };
+    }
 }
 
 export function startNewGame() {
+    const positions = range(startRow * COLS, SIZE, 1).sort(() => 0.5 - rand.random())
     return {
-        type: NEW_GAME
-    };
+        type: NEW_GAME,
+        positions
+    }
 }
 
 export function gameOver() {
     return {
         type: GAME_OVER
-    };
+    }
 }
 
