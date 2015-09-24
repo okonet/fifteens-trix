c752be6f30f264d44df0b9079e2f6f6a3e77e8d2
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-09-24 20:37:10 +0200

    Implemented proper playTile with check if it's a neighbor of the empty tile

--- src/actions/board.js
+++ src/actions/board.js
@@ -1,21 +1,26 @@
-export const PLAY_TILE = 'PLAY_TILE';
-export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+import { COLS } from '../constants/Board'
+export const PLAY_TILE = 'PLAY_TILE'
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
 
-export function playTile(position) {
-    return {
-        type: PLAY_TILE,
-        position
+export function playTile(position: number) {
+    return (dispatch, getState) => {
+        const { tiles } = getState()
+        const emptyTile = tiles.find(tile => tile.type === -1)
+        const emptyPos = tiles.indexOf(emptyTile)
+        const delta = Math.abs(position - emptyPos)
+
+        if (delta === 1 || delta === COLS) {
+            if (emptyPos % COLS === 0 && position === (emptyPos - 1)) {
+                return // Empty is in last row. Next tile can not be played
+            }  else if ((emptyPos + 1) % COLS === 0 && position === (emptyPos + 1)) {
+                return // Empty is in first row. Previous tile can not be played
+            }
+            dispatch({
+                type: PLAY_TILE,
+                position
+            });
+        }
+
+        return
     };
 }
-
-//export function playTile(position: number) {
-//    return (dispatch, getState) => {
-//        const { tiles } = getState();
-//
-//        if (counter % 2 === 0) {
-//            return;
-//        }
-//
-//        dispatch(increment());
-//    };
-//}
