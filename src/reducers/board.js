c747fb84244d762601a7484f570a889248de5fe4
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-09-30 18:29:28 +0200

    Implemented updatePlayableTiles to mark playable tiles with attribute.

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -6,6 +6,22 @@ import { COLS, ROWS, SIZE } from '../constants/Board'
 const initialState = []
 const startRow = (ROWS - COLS)
 
+function getEmptyTilePos(items) {
+    return items.indexOf(findWhere(items, {index: SIZE - 1}))
+}
+
+function updatePlayableTiles(items) {
+    const emptyIdx = getEmptyTilePos(items)
+    const emptyRow = ~~(emptyIdx / COLS)
+    return items.map((item, idx) => {
+        const delta = (idx - emptyIdx)
+        const isSameCol = Math.abs(delta) % COLS === 0
+        const isSameRow = ~~(idx / COLS) === emptyRow
+        const isPlayable = isSameCol || isSameRow
+        return {...item, isPlayable}
+    })
+}
+
 export default function tiles(state = initialState, action) {
     switch (action.type) {
         case NEW_GAME:
@@ -17,22 +33,23 @@ export default function tiles(state = initialState, action) {
             for (let row = startRow; row < ROWS; row++) {
                 for (let col = 0; col < COLS; col++) {
                     const idx = row * COLS + col
+                    const isEmpty = idx === (SIZE - 1)
                     filledBoard.push({
-                        type: idx === (SIZE - 1)  ? -1 : row - startRow + 1,
+                        type: isEmpty ? -1 : row - startRow + 1,
                         index: idx
                     })
                 }
             }
             filledBoard = sortBy(filledBoard, (item, idx) => action.positions[idx])
 
-            return [...emptyBoard, ...filledBoard]
+            return updatePlayableTiles([...emptyBoard, ...filledBoard])
 
         case PLAY_TILE:
             const { position } = action
-            const emptyTile = findWhere(state, {index: SIZE - 1})
-            const emptyTilePosition = state.indexOf(emptyTile)
+
+            const emptyTilePosition = getEmptyTilePos(state)
             state[emptyTilePosition] = state.splice(position, 1, state[emptyTilePosition])[0]
-            return [...state]
+            return updatePlayableTiles([...state])
 
         default:
             return state
