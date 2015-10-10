23617db6692ce2c726617d041f3ae6295843c118
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-10-10 13:34:20 +0200

    Added touch handlers + updated code to react-motion 0.3

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Spring } from 'react-motion'
+import { Motion, spring, presets } from 'react-motion'
 import { COLS } from '../constants/Board'
 
 const TILE_SIZE = 60
@@ -23,38 +23,84 @@ export default class Tile extends Component {
         isPlayable: false
     }
 
+    state = {
+        offsetX: 0,
+        offsetY: 0
+    }
+
     onClick(position) {
         if (this.props.onClickCb) {
             this.props.onClickCb(position)
         }
     }
 
-    getLeftValue(position) {
-        return (position % COLS) * TILE_SIZE
+    onTouchStart = (evt) => {
+        const touch = evt.touches[0]
+        this.initialTouchPosition = {
+            x: touch.clientX,
+            y: touch.clientY
+        }
+        this.setState({
+            isTouched: true
+        })
     }
 
-    getTopValue(position) {
-        return (Math.ceil((position + 1) / COLS) - 1) * TILE_SIZE
+    onTouchMove = (evt) => {
+        const touch = evt.touches[0]
+        if (this.initialTouchPosition) {
+            this.setState({
+                offsetX: touch.clientX - this.initialTouchPosition.x,
+                offsetY: touch.clientY - this.initialTouchPosition.y
+            })
+        }
+    }
+
+    onTouchEnd = () => {
+        this.initialTouchPosition = null
+        this.setState({
+            isTouched: false,
+            offsetX: 0,
+            offsetY: 0
+        })
+    }
+
+    getLeftValue(position: number): number {
+        const {offsetX} = this.state
+        return offsetX + (position % COLS) * TILE_SIZE
+    }
+
+    getTopValue(position: number): number {
+        const {offsetY} = this.state
+        return offsetY + (Math.ceil((position + 1) / COLS) - 1) * TILE_SIZE
+    }
+
+    getPosition(position: number): Object {
+        const springSettings = presets.stiff
+        return {
+            left: spring(this.getLeftValue(position), springSettings),
+            top: spring(this.getTopValue(position), springSettings)
+        }
     }
 
     render() {
         const {type, position, isPlayable} = this.props
         return (
-            <Spring
-                endValue={{val: {left: this.getLeftValue(position), top: this.getTopValue(position)}}}
-            >
-                {interpolated =>
+            <Motion style={this.getPosition(position)}>
+                {interpolatedStyle =>
                     <div
                         style={{
                             ...basicStyles,
                             opacity: type <= 0 ? 0.1 : 1,
                             borderColor: isPlayable ? '#0c0' : '#000',
-                            transform: `translate(${interpolated.val.left}px, ${interpolated.val.top}px)`
+                            transform: `translate(${interpolatedStyle.left}px, ${interpolatedStyle.top}px)`
                         }}
                         onClick={this.onClick.bind(this, position)}
+                        onTouchStart={this.onTouchStart}
+                        onTouchMove={this.onTouchMove}
+                        onTouchEnd={this.onTouchEnd}
                     >{type}</div>
                 }
-            </Spring>
+            </Motion>
         )
     }
 }
