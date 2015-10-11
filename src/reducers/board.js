18a0500be217f84e7f87d58347158a0578e40344
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-10-11 14:36:28 +0200

    Use position attr instead of manupulating the array. Added tests

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,4 +1,4 @@
-import { range, findWhere, sortBy } from 'lodash'
+import { range, findWhere, pluck } from 'lodash'
 import { NEW_GAME } from '../actions/game'
 import { PLAY_TILE } from '../actions/board'
 import { COLS, ROWS, SIZE } from '../constants/Board'
@@ -6,18 +6,18 @@ import { COLS, ROWS, SIZE } from '../constants/Board'
 const initialState = []
 const startRow = (ROWS - COLS)
 
-function getEmptyTilePos(items) {
-    return items.indexOf(findWhere(items, {index: SIZE - 1}))
+export function getEmptyTilePos(items: Array<Object>): number {
+    return findWhere(items, {type: -1}).position
 }
 
-function updatePlayableTiles(items) {
-    const emptyIdx = getEmptyTilePos(items)
-    const emptyRow = ~~(emptyIdx / COLS)
-    return items.map((item, idx) => {
-        const delta = (idx - emptyIdx)
-        const isSameCol = Math.abs(delta) % COLS === 0
-        const isSameRow = ~~(idx / COLS) === emptyRow
-        const isPlayable = isSameCol || isSameRow
+export function updatePlayableTiles(items: Array<Object>, cols: number = COLS): Array<Object> {
+    const emptyPos = getEmptyTilePos(items)
+    const emptyRow = ~~(emptyPos / cols)
+    return items.map(item => {
+        const delta = (item.position - emptyPos)
+        const isSameCol = Math.abs(delta) % cols === 0
+        const isSameRow = ~~(item.position / cols) === emptyRow
+        const isPlayable = item.type > 0 && (isSameCol || isSameRow)
         return {...item, isPlayable}
     })
 }
@@ -25,31 +25,57 @@ function updatePlayableTiles(items) {
 export default function tiles(state = initialState, action) {
     switch (action.type) {
         case NEW_GAME:
-            const emptyBoard = range(0, startRow * COLS, 1).map(index => {
-                return {type: 0, index}
+            const emptyBoard = range(0, startRow * COLS, 1).map(position => {
+                return {type: 0, position}
             })
 
             let filledBoard = []
             for (let row = startRow; row < ROWS; row++) {
                 for (let col = 0; col < COLS; col++) {
-                    const idx = row * COLS + col
-                    const isEmpty = idx === (SIZE - 1)
+                    const position = row * COLS + col
+                    const isEmpty = position === (SIZE - 1)
                     filledBoard.push({
                         type: isEmpty ? -1 : row - startRow + 1,
-                        index: idx
+                        position
                     })
                 }
             }
-            filledBoard = sortBy(filledBoard, (item, idx) => action.positions[idx])
+            filledBoard = filledBoard.map((item, idx) => {
+                return {
+                    ...item,
+                    position: action.positions[idx]
+                }
+            })
 
             return updatePlayableTiles([...emptyBoard, ...filledBoard])
 
         case PLAY_TILE:
             const { position } = action
-
             const emptyTilePosition = getEmptyTilePos(state)
-            state[emptyTilePosition] = state.splice(position, 1, state[emptyTilePosition])[0]
-            return updatePlayableTiles([...state])
+            const tileToPlay = findWhere(state, {position})
+            if (tileToPlay.isPlayable) {
+                let positions = pluck(state, 'position')
+
+                // Switch positions
+                positions = positions.map(pos => {
+                    if (pos === emptyTilePosition) {
+                        return position
+                    }
+                    if (pos === position) {
+                        return emptyTilePosition
+                    }
+                    return pos
+                })
+
+                const newState = state.map((tile, idx) => {
+                    return {
+                        ...tile,
+                        position: positions[idx]
+                    }
+                })
+                return updatePlayableTiles(newState)
+            }
+            return state
 
         default:
             return state
