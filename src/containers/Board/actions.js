8927a6fafc8bd18798c1195d8a928f3d40cda8ba
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-01 14:36:35 +0200

    Do not check if tile is playable in the action and just dispatch it

--- src/containers/Board/actions.js
+++ src/containers/Board/actions.js
@@ -1,17 +1,8 @@
-import { find } from 'lodash'
-
 export const PLAY_TILE = 'PLAY_TILE'
 
 export function playTile(position: number) {
-  return (dispatch, getState) => {
-    const { board } = getState()
-    const tile = find(board, { position })
-
-    if (tile.isPlayable) {
-      dispatch({
-        type: PLAY_TILE,
-        position
-      })
-    }
+  return {
+    type: PLAY_TILE,
+    position
   }
 }
