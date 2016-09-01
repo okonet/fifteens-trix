7b09d93468cd79d4265a2e32cbfb44bd537a9790
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-01 15:04:52 +0200

    Implement increment of moves

--- src/containers/Board/actions.js
+++ src/containers/Board/actions.js
@@ -1,17 +1,19 @@
-import { find } from 'lodash'
+import { getTileWithPosition } from './reducer'
+import { incrementMoves } from '../Game/actions'
 
 export const PLAY_TILE = 'PLAY_TILE'
 
 export function playTile(position: number) {
   return (dispatch, getState) => {
     const { board } = getState()
-    const tile = find(board, { position })
+    const tile = getTileWithPosition(board, position)
 
     if (tile.isPlayable) {
       dispatch({
         type: PLAY_TILE,
         position
       })
+      dispatch(incrementMoves())
     }
   }
 }
