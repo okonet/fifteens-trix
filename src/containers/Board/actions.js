5227011f2597c0910c4641d708f04e64dedecdeb
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-01 15:02:16 +0200

    Revert "Do not check if tile is playable in the action and just dispatch it"
    This reverts commit 8927a6fafc8bd18798c1195d8a928f3d40cda8ba.

--- src/containers/Board/actions.js
+++ src/containers/Board/actions.js
@@ -1,8 +1,17 @@
+import { find } from 'lodash'
+
 export const PLAY_TILE = 'PLAY_TILE'
 
 export function playTile(position: number) {
-  return {
-    type: PLAY_TILE,
-    position
+  return (dispatch, getState) => {
+    const { board } = getState()
+    const tile = find(board, { position })
+
+    if (tile.isPlayable) {
+      dispatch({
+        type: PLAY_TILE,
+        position
+      })
+    }
   }
 }
