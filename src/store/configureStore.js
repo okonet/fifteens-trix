e19e063fc5b40754cc4acf53a0d2d5f413491677
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-01 12:28:43 +0200

    Added devToolsExtension and fixed babel issues with HMR

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,17 +2,16 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from '../reducers'
 
-const createStoreWithMiddleware = compose(
-    applyMiddleware(thunk)
-)(createStore)
-
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(reducer, initialState)
+  const store = createStore(reducer, initialState, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
 
   if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
+    // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-            const nextReducer = require('../reducers') // eslint-disable-line
+      const nextReducer = require('../reducers').default // eslint-disable-line
       store.replaceReducer(nextReducer)
     })
   }
