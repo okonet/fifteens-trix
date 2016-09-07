335b5c479f59143e5b741691fd73e586593a05b6
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-09-08 00:06:00 +0200

    Fixed destructuring

--- src/containers/Board/actions.js
+++ src/containers/Board/actions.js
@@ -5,9 +5,8 @@ export const PLAY_TILE = 'PLAY_TILE'
 
 export function playTile(position: number) {
   return (dispatch, getState) => {
-    const { board } = getState()
-    const tile = getTileWithPosition(board, position)
-
+    const { tiles } = getState().board
+    const tile = getTileWithPosition(tiles, position)
     if (tile.isPlayable) {
       dispatch({
         type: PLAY_TILE,
